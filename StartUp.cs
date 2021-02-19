771d415388e2b945b1ebb9918d5e2f5b2db75065
Vishw Patel <vishwpatel511@gmail.com>
2021-02-19 08:52:40 -0500

    Created StartUp class for OWIN

---
+++ StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+// required for OWIN startUp
+using Microsoft.AspNet.Identity;
+using  Microsoft.Owin.Security.Cookies;
+
+
+[assembly: OwinStartup(typeof(COMP229_S2016_Lesson2.StartUp))]
+
+namespace COMP229_S2016_Lesson2
+{
+    public class StartUp
+    {
+        public void Configuration(IAppBuilder app)
+        {
+          
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Login.aspx")
+            });
+
+        }
+    }
+}
